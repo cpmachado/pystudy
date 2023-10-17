064929c4f7591566abedac4c94a7243e47c0a53c
cpmachado <cpmachado@protonmail.com>
2023-10-17 22:35:02 +0100

    Combining Positional and Optional arguments

--- howto/argparse/square.py
+++ howto/argparse/square.py
@@ -1,6 +1,15 @@
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("square", help="display a square of a given number", type=int)
+parser.add_argument("square", type=int, help="display a square of a given number")
+parser.add_argument(
+    "-v", "--verbosity", action="count", default=0, help="increase output verbosity"
+)
 args = parser.parse_args()
-print(args.square**2)
+answer = args.square**2
+if args.verbosity >= 2:
+    print(f"the square of {args.square} equals {answer}")
+elif args.verbosity >= 1:
+    print(f"{args.square}^2 == {answer}")
+else:
+    print(answer)
